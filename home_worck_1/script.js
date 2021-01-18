4e752050e539fb74da3c1277ac7a941d2d15823d
multicalor <d.tretiuk@gmil.com>
2021-01-18 22:13:43 +0200

    fix:

--- home_worck_1/script.js
+++ home_worck_1/script.js
@@ -1,7 +1,10 @@
 alert("Hello!");
-let name = prompt("What is your name?")
+
+let name = prompt("What is your name?");
+
 while (name === ''){
-    name = prompt("Plyse enter yor name!")
-}
-alert(`Nice to meet you, ${name}`)
+    name = prompt("Plyse enter yor name!");
+};
+
+alert(`Nice to meet you, ${name}`);
 
