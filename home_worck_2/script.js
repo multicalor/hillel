ee3e4d3c6967f664c132367c64faaaa8b6a6a916
multicalor <d.tretiuk@gmil.com>
2021-01-21 22:11:49 +0200

    complete home worck 2

--- home_worck_2/script.js
+++ home_worck_2/script.js
@@ -1,24 +1,37 @@
 alert("Hello!");
 
-let leftOperand = +prompt("Enter number"); // не явное преобразование
+let firstOperand = +prompt("Enter first operand"); // не явное преобразование
 
-let rightOperand = Number(prompt("Enter number")); //явное преобразование
+let secondOperand = Number(prompt("Enter second operand")); //явное преобразование
+
+let resultString = 'Calculations are finished!\n'
+
+// Calculations are finished!
+// Sum: 2 + 2 = 4
+// Diff: 2 - 2 = 0
+// Mult: 2 * 2 = 4
+// Div: 2 / 2 = 1
+
+
+// Console result
+
+console.log("Calculations are finished!");
+// Sum
+console.log(`Sum: ${firstOperand} + ${secondOperand} = ${firstOperand + secondOperand}`);
+// Diff
+console.log(`Sum: ${firstOperand} - ${secondOperand} = ${firstOperand - secondOperand}`);
+// Mult
+console.log(`Sum: ${firstOperand} * ${secondOperand} = ${firstOperand * secondOperand}`);
+// Div
+console.log(`Sum: ${firstOperand} / ${secondOperand} = ${firstOperand / secondOperand}`);
+
+resultString+= `Sum: ${firstOperand} + ${secondOperand} = ${firstOperand + secondOperand}\n`
+resultString+= `Diff: ${firstOperand} - ${secondOperand} = ${firstOperand - secondOperand}\n`
+resultString+= `Mult: ${firstOperand} * ${secondOperand} = ${firstOperand * secondOperand}\n`
+resultString+= `Div: ${firstOperand} / ${secondOperand} = ${firstOperand / secondOperand}`
+
+alert(resultString);
 
 
 
-console.log(leftOperand + rightOperand);
-
-alert(leftOperand + rightOperand);
-
-console.log(leftOperand - rightOperand);
-
-alert(leftOperand - rightOperand);
-
-console.log(leftOperand * rightOperand);
-
-alert(leftOperand * rightOperand);
-
-console.log(leftOperand / rightOperand);
-
-alert(leftOperand / rightOperand);
 
