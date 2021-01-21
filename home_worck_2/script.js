1b46435b65147c6ad053bac2f787878cd988409e
multicalor <d.tretiuk@gmil.com>
2021-01-21 21:32:58 +0200

    home worck 2

---
+++ home_worck_2/script.js
@@ -0,0 +1,24 @@
+alert("Hello!");
+
+let leftOperand = +prompt("Enter number"); // не явное преобразование
+
+let rightOperand = Number(prompt("Enter number")); явное преобразование
+
+
+
+console.log(leftOperand + rightOperand);
+
+alert(leftOperand + rightOperand);
+
+console.log(leftOperand - rightOperand);
+
+alert(leftOperand - rightOperand);
+
+console.log(leftOperand * rightOperand);
+
+alert(leftOperand * rightOperand);
+
+console.log(leftOperand / rightOperand);
+
+alert(leftOperand / rightOperand);
+
